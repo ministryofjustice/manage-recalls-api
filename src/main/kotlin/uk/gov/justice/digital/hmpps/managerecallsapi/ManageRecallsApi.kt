888fd779c44b0e873f74fd1a53eb739c0cb3f018
Darren Oakley <darren.oakley@digital.justice.gov.uk>
2021-05-26 16:45:33 +0100

    Run the 'rename-project.bash' script (#1)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/managerecallsapi/ManageRecallsApi.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/managerecallsapi/ManageRecallsApi.kt
@@ -1,11 +1,11 @@
-package uk.gov.justice.digital.hmpps.hmppstemplatepackagename
+package uk.gov.justice.digital.hmpps.managerecallsapi
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication()
-class HmppsTemplateKotlin
+class ManageRecallsApi
 
 fun main(args: Array<String>) {
-  runApplication<HmppsTemplateKotlin>(*args)
+  runApplication<ManageRecallsApi>(*args)
 }
