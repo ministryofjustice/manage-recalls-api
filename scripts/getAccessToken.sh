460d791986776b130672dba6c77222f08e5d8631
Mike Dunford <mike.dunford@digital.justice.gov.uk>
2021-06-21 10:47:51 +0100

    Add noddy script to get API access token

---
+++ scripts/getAccessToken.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -euo pipefail
+
+readonly SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+readonly PROJECT_DIR="${SCRIPT_DIR}/.."
+readonly SECRET_NAME=manage-recalls-api
+readonly NAMESPACE=ppud-replacement-dev
+readonly HMPPS_AUTH_URL=https://sign-in-dev.hmpps.service.justice.gov.uk
+
+kubectl config set-context --current --namespace=$NAMESPACE
+
+readonly secrets=`kubectl get secret manage-recalls-api -o json`
+readonly client_id=`jq -r '.data.API_CLIENT_ID' <<< "${secrets}" | base64 -d`
+readonly client_secret=`jq -r '.data.API_CLIENT_SECRET' <<< "${secrets}" | base64 -d`
+readonly access_token=`curl -s -X POST "${HMPPS_AUTH_URL}/auth/oauth/token?grant_type=client_credentials" \
+  -H 'Content-Type: application/json' \
+  -H "Authorization: Basic $(echo -n ${client_id}:${client_secret} | base64)"  | jq -r .access_token`
+
+echo $access_token
