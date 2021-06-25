231df07e4a66985cc26e348fbd9c6ce02a90f012
NataliaOsk <natalia.oskina@equalexperts.com>
2021-06-25 14:34:49 +0100

    PUD-78: [MD, NO] Refactor pdf generation endpoint to return Base64 string

--- src/test/kotlin/uk/gov/justice/digital/hmpps/managerecallsapi/integration/documents/GenerateRevocationOrderTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/managerecallsapi/integration/documents/GenerateRevocationOrderTest.kt
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import uk.gov.justice.digital.hmpps.managerecallsapi.Pdf
 import uk.gov.justice.digital.hmpps.managerecallsapi.integration.IntegrationTestBase
 import uk.gov.justice.digital.hmpps.managerecallsapi.integration.mockservers.GotenbergMockServer
+import java.util.Base64
 
 class GenerateRevocationOrderTest : IntegrationTestBase() {
 
@@ -35,11 +36,12 @@ class GenerateRevocationOrderTest : IntegrationTestBase() {
   @Test
   fun `generate revocation order endpoint should return generated pdf`() {
     val expectedPdf = "Expected Generated PDF".toByteArray()
+    val expectedBase64Pdf = Base64.getEncoder().encodeToString(expectedPdf)
     gotenbergMockServer.stubPdfGeneration(expectedPdf)
 
     val response = authenticatedPostRequest("/generate-revocation-order")
 
-    assertThat("Generated PDF does not match", response.content.contentEquals(expectedPdf), equalTo(true))
+    assertThat("Generated PDF does not match", response.content, equalTo(expectedBase64Pdf))
   }
 
   private fun authenticatedPostRequest(path: String): Pdf =
