3bd76d07c24bb735810b95c958bd07645c655d93
Mike Dunford <mike.dunford@digital.justice.gov.uk>
2021-06-28 14:00:02 +0100

    PUD-44: [MD, NO] Retrieve document parts for revocation order using inputstream.  Add fake-prisoner-offender-search

--- src/main/kotlin/uk/gov/justice/digital/hmpps/managerecallsapi/documents/PdfDocumentGenerator.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/managerecallsapi/documents/PdfDocumentGenerator.kt
@@ -4,15 +4,20 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.core.io.ClassPathResource
-import org.springframework.http.MediaType
+import org.springframework.core.io.InputStreamResource
+import org.springframework.http.HttpHeaders.CONTENT_DISPOSITION
+import org.springframework.http.MediaType.MULTIPART_FORM_DATA
 import org.springframework.http.client.MultipartBodyBuilder
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
+import java.io.IOException
+import java.io.InputStream
 
 @Component
 class PdfDocumentGenerator(
   private val webClient: WebClient,
-  @Value("\${gotenberg.endpoint.url}") private val gotenbergEndpointUrl: String
+  @Value("\${gotenberg.endpoint.url}") private val gotenbergEndpointUrl: String,
+  private val revocationOrder: RevocationOrder
 ) {
 
   companion object {
@@ -20,22 +25,45 @@ class PdfDocumentGenerator(
   }
 
   fun makePdf(): ByteArray {
-    val bodyBuilder = MultipartBodyBuilder()
-
-    bodyBuilder
-      .part("index.html", ClassPathResource("/document/template/revocation-order/index.html").file.readText())
-      .header("Content-Disposition", "form-data; name=index.html; filename=index.html")
-    bodyBuilder
-      .part("logo.png", ClassPathResource("/document/template/revocation-order/logo.png").file.readBytes())
-      .header("Content-Disposition", "form-data; name=logo.png; filename=logo.png")
+    val documentBody = MultipartBodyBuilder().apply {
+      revocationOrder.details.forEach { documentDetail ->
+        this
+          .part(documentDetail.name, documentDetail.inputStream())
+          .header(CONTENT_DISPOSITION, "form-data; name=${documentDetail.name}; filename=${documentDetail.name}")
+      }
+    }.build()
 
     return webClient
       .post()
       .uri("$gotenbergEndpointUrl/convert/html")
-      .contentType(MediaType.MULTIPART_FORM_DATA)
-      .bodyValue(bodyBuilder.build())
+      .contentType(MULTIPART_FORM_DATA)
+      .bodyValue(documentBody)
       .retrieve()
       .bodyToMono(ByteArray::class.java)
       .block()!!
+  }
+}
+
+@Component
+class RevocationOrder {
+  val details = listOf(
+    DocumentDetail("index.html", "/document/template/revocation-order/index.html"),
+    DocumentDetail("logo.png", "/document/template/revocation-order/logo.png")
+  )
+}
+
+data class DocumentDetail(val name: String, val path: String) {
+  fun inputStream() = MultipartInputStreamFileResource(ClassPathResource(path).inputStream, name)
+}
+
+class MultipartInputStreamFileResource(inputStream: InputStream, private val filename: String) :
+  InputStreamResource(inputStream) {
+  override fun getFilename(): String {
+    return filename
+  }
+
+  @Throws(IOException::class)
+  override fun contentLength(): Long {
+    return -1
   }
 }
