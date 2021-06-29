4c8bd173b131b3c6a07703f4063f8be7020a6555
Mike Dunford <mike.dunford@digital.justice.gov.uk>
2021-06-29 16:38:41 +0100

    PUD-187: [MD, NO] Run document generation tests in circleCI

--- scripts/run-docker-integration-tests.sh
+++ scripts/run-docker-integration-tests.sh
@@ -4,11 +4,10 @@ set -euo pipefail
 
 readonly SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 readonly PROJECT_DIR="${SCRIPT_DIR}/.."
-readonly DOCKER_COMPOSE_FILE="docker-compose-test.yml"
-readonly DOCKER_COMPOSE_INT_TESTS_FILE="docker-compose-ci-tests.yml"
+readonly DOCKER_COMPOSE_INT_TESTS_FILE="docker-compose-int-test.yml"
 
 pushd ${PROJECT_DIR}
-docker compose -f "${DOCKER_COMPOSE_FILE}" -f "${DOCKER_COMPOSE_INT_TESTS_FILE}" build
-docker compose -f "${DOCKER_COMPOSE_FILE}" -f "${DOCKER_COMPOSE_INT_TESTS_FILE}" pull
-docker compose -f "${DOCKER_COMPOSE_FILE}" -f "${DOCKER_COMPOSE_INT_TESTS_FILE}" up --exit-code-from manage-recalls-api-int-tests
+docker compose -f "${DOCKER_COMPOSE_INT_TESTS_FILE}" build
+docker compose -f "${DOCKER_COMPOSE_INT_TESTS_FILE}" pull
+docker compose -f "${DOCKER_COMPOSE_INT_TESTS_FILE}" up --exit-code-from manage-recalls-api-int-tests
 popd
--- scripts/start-gotenburg.sh
+++ scripts/start-gotenburg.sh
@@ -6,8 +6,8 @@ readonly SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 readonly PROJECT_DIR="${SCRIPT_DIR}/.."
 readonly MANAGE_RECALLS_SERVICE_NAME='manage-recalls-api'
 readonly LOG_FILE="/tmp/${MANAGE_RECALLS_SERVICE_NAME}.log"
-readonly DOCKER_COMPOSE_FILE="$PROJECT_DIR/docker-compose-test.yml"
+readonly DOCKER_COMPOSE_FILE="$PROJECT_DIR/docker-compose-int-test.yml"
 
-echo "Starting gotenburg"
+echo "Starting gotenberg"
 docker compose -f "${DOCKER_COMPOSE_FILE}" pull
-docker compose -f "${DOCKER_COMPOSE_FILE}" up -d --remove-orphans
+docker compose -f "${DOCKER_COMPOSE_FILE}" up gotenberg -d --remove-orphans
