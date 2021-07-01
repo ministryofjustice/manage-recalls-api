14c9d19fb2e4d0e2a07fbba75b5895d59973537a
Mike Dunford <mike.dunford@digital.justice.gov.uk>
2021-07-01 14:43:55 +0100

    PUD-94 [MD PS]:  Add DB status to HealthCheckTest

--- scripts/stop-gotenburg.sh
+++ scripts/stop-gotenburg.sh
@@ -4,9 +4,6 @@ set -euo pipefail
 
 readonly SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 readonly PROJECT_DIR="${SCRIPT_DIR}/.."
-readonly DOCKER_COMPOSE_FILE="$PROJECT_DIR/docker-compose-test.yml"
+readonly DOCKER_COMPOSE_FILE="$PROJECT_DIR/docker-compose-int-test.yml"
 
 docker compose -f $DOCKER_COMPOSE_FILE stop
-
-#TODO replace without npx
-#npx kill-port 8080
