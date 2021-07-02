97e7a5ea816853a3ca9cc7cbe9eebb587b6c8dfa
Phil Segal <philsegal@users.noreply.github.com>
2021-07-02 15:55:26 +0100

    PUD-191: Book recall endpoint (#35)

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/managerecallsapi/controller/RecallsController.kt
@@ -0,0 +1,37 @@
+package uk.gov.justice.digital.hmpps.managerecallsapi.controller
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.RestController
+import uk.gov.justice.digital.hmpps.managerecallsapi.db.Recall
+import uk.gov.justice.digital.hmpps.managerecallsapi.db.RecallRepository
+import java.util.UUID
+
+@RestController
+@RequestMapping(produces = [MediaType.APPLICATION_JSON_VALUE])
+@PreAuthorize("hasRole('ROLE_MANAGE_RECALLS')")
+class RecallsController(
+  @Autowired private val recallRepository: RecallRepository
+) {
+
+  @PostMapping("/recalls")
+  @ResponseBody
+  fun bookRecall(@RequestBody bookRecallRequest: BookRecallRequest) =
+    ResponseEntity(
+      recallRepository.save(bookRecallRequest.toRecall()).toResponse(), HttpStatus.CREATED
+    )
+}
+
+fun BookRecallRequest.toRecall() = Recall(UUID.randomUUID(), this.nomsNumber)
+
+fun Recall.toResponse() = BookRecallResponse(this.id, this.nomsNumber)
+
+data class BookRecallRequest(val nomsNumber: String)
+data class BookRecallResponse(val id: UUID, val nomsNumber: String)
---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/managerecallsapi/controller/RecallsControllerTest.kt
@@ -0,0 +1,27 @@
+package uk.gov.justice.digital.hmpps.managerecallsapi.controller
+
+import com.natpryce.hamkrest.assertion.assertThat
+import com.natpryce.hamkrest.equalTo
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.jupiter.api.Test
+import uk.gov.justice.digital.hmpps.managerecallsapi.db.RecallRepository
+
+class RecallsControllerTest {
+  private val recallRepository = mockk<RecallRepository>()
+
+  private val underTest = RecallsController(recallRepository)
+
+  private val nomsNumber = "A1234AA"
+  private val recallRequest = BookRecallRequest(nomsNumber)
+
+  @Test
+  fun `book recall returns request with id`() {
+    val recall = recallRequest.toRecall()
+    every { recallRepository.save(any()) } returns recall
+
+    val results = underTest.bookRecall(recallRequest)
+
+    assertThat(results.body, equalTo(BookRecallResponse(recall.id, nomsNumber)))
+  }
+}
