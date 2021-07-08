177fab87be14cc14d77aafacc07bf977bd1e016a
Mike Dunford <mike.dunford@digital.justice.gov.uk>
2021-07-08 09:54:32 +0100

    PUD-101: Add initial pact provider test for manage-recalls-ui

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,19 +9,22 @@ repositories {
 }
 
 configurations {
-  testImplementation { exclude(group = "org.junit.vintage") }
+  testImplementation {
+    exclude(group = "org.junit.vintage")
+    exclude(group = "com.vaadin.external.google", module = "android-json")
+  }
 }
 
 dependencies {
-  implementation("org.springframework.boot:spring-boot-starter-webflux")
-  implementation("org.springframework.boot:spring-boot-starter-security")
+  implementation("org.springframework.boot:spring-boot-starter-data-jpa")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+  implementation("org.springframework.boot:spring-boot-starter-security")
+  implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springframework.security:spring-security-oauth2-client")
 
   implementation("io.springfox:springfox-boot-starter:3.0.0")
   implementation("org.flywaydb:flyway-core:7.10.0")
   implementation("org.postgresql:postgresql:42.2.22")
-  implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 
   testImplementation("io.jsonwebtoken:jjwt:0.9.1")
 
@@ -39,6 +42,13 @@ dependencies {
   testImplementation("io.zonky.test:embedded-postgres:1.3.0")
   testImplementation("io.projectreactor:reactor-test")
   testImplementation("com.ninja-squad:springmockk:3.0.1")
+
+  testImplementation("au.com.dius:pact-jvm-consumer-junit_2.11:3.5.24")
+  testImplementation("au.com.dius:pact-jvm-consumer-junit_2.11:3.5.24")
+
+  testImplementation("au.com.dius.pact.provider:junit5:4.1.4")
+  testImplementation("au.com.dius.pact.provider:junit5spring:4.2.7")
+  testImplementation("au.com.dius:pact-jvm-provider-junit5:4.0.10")
 }
 
 tasks {
@@ -54,7 +64,7 @@ tasks {
 }
 
 task<Test>("documentGenerationTest") {
-  description = "Runs the documetn generation tests against gotenberg"
+  description = "Runs the document generation tests against gotenberg"
   group = "verification"
   testClassesDirs = sourceSets["test"].output.classesDirs
   classpath = sourceSets["test"].runtimeClasspath
