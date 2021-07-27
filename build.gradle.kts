17819d0b0f0a758175357a2244c63adaa42f4255
Ken Friesen <ken.friesen@digital.justice.gov.uk>
2021-07-27 15:59:32 +0100

    NJ Append console logs in Logstash format
    So that the logs can be indexed correctly in Elasticsearch and
    thus can be filtered appropriately in Kibana

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,18 +20,16 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
   implementation("org.springframework.boot:spring-boot-starter-security")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
+  implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+  implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
   implementation("org.springframework.security:spring-security-oauth2-client")
 
   implementation("io.springfox:springfox-boot-starter:3.0.0")
   implementation("org.flywaydb:flyway-core:7.10.0")
   implementation("org.postgresql:postgresql:42.2.22")
-  implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-  implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
-
   implementation("software.amazon.awssdk:s3:2.16.101")
   implementation("org.http4k:http4k-format-jackson:4.10.0.1")
-
-  testImplementation("io.jsonwebtoken:jjwt:0.9.1")
+  implementation("net.logstash.logback:logstash-logback-encoder:6.6")
 
   "5.7.0".let { junitVersion ->
     testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
@@ -39,6 +37,7 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-params:$junitVersion")
   }
 
+  testImplementation("io.jsonwebtoken:jjwt:0.9.1")
   testImplementation("io.mockk:mockk:1.10.0")
   testImplementation("com.natpryce:hamkrest:1.7.0.3")
   testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
@@ -47,7 +46,6 @@ dependencies {
   testImplementation("io.zonky.test:embedded-postgres:1.3.0")
   testImplementation("io.projectreactor:reactor-test")
   testImplementation("com.ninja-squad:springmockk:3.0.1")
-
   testImplementation("au.com.dius.pact.provider:junit5:4.2.7")
   testImplementation("au.com.dius.pact.provider:junit5spring:4.2.7")
 }
@@ -73,7 +71,10 @@ tasks {
     systemProperty("pact.provider.tag", System.getenv("PACT_PROVIDER_TAG"))
     systemProperty("pact.provider.version", System.getenv("PACT_PROVIDER_VERSION"))
     systemProperty("pact.verifier.publishResults", System.getenv("PACT_PUBLISH_RESULTS") ?: "false")
-    systemProperty("pactbroker.host", System.getenv("PACTBROKER_HOST") ?: "pact-broker-prod.apps.live-1.cloud-platform.service.justice.gov.uk")
+    systemProperty(
+      "pactbroker.host",
+      System.getenv("PACTBROKER_HOST") ?: "pact-broker-prod.apps.live-1.cloud-platform.service.justice.gov.uk"
+    )
 
     useJUnitPlatform {
       include("**/*PactTest*")
