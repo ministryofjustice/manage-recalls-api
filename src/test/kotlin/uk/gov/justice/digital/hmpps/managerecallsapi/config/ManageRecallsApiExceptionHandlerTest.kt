2b82e230f56dd6b21a9c4be9c6c28000f136b357
Mike Dunford <mike.dunford@digital.justice.gov.uk>
2021-07-29 09:39:49 +0100

    PUD-26:  Delete unused exception handlers

--- src/test/kotlin/uk/gov/justice/digital/hmpps/managerecallsapi/config/ManageRecallsApiExceptionHandlerTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/managerecallsapi/config/ManageRecallsApiExceptionHandlerTest.kt
@@ -6,18 +6,11 @@ import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
 import com.natpryce.hamkrest.has
 import com.natpryce.hamkrest.present
-import io.mockk.mockk
 import org.junit.jupiter.api.Test
 import org.springframework.http.HttpStatus
-import org.springframework.http.HttpStatus.BAD_REQUEST
 import org.springframework.http.HttpStatus.NOT_FOUND
 import org.springframework.http.ResponseEntity
 import org.springframework.orm.jpa.JpaObjectRetrievalFailureException
-import org.springframework.validation.BeanPropertyBindingResult
-import org.springframework.validation.BindingResult
-import org.springframework.validation.FieldError
-import org.springframework.validation.ObjectError
-import org.springframework.web.bind.MethodArgumentNotValidException
 import uk.gov.justice.digital.hmpps.managerecallsapi.domain.RecallId
 import uk.gov.justice.digital.hmpps.managerecallsapi.domain.random
 import uk.gov.justice.digital.hmpps.managerecallsapi.service.RecallNotFoundException
@@ -26,7 +19,6 @@ import javax.persistence.EntityNotFoundException
 class ManageRecallsApiExceptionHandlerTest {
 
   private val underTest = ManageRecallsApiExceptionHandler()
-  private val objectName = "objectName"
 
   @Test
   fun `NotFoundExceptions should be translated to 400`() {
@@ -38,57 +30,6 @@ class ManageRecallsApiExceptionHandlerTest {
     assertThat(result, isResponseEntityMatching(NOT_FOUND, ErrorResponse(NOT_FOUND, errorMessage)))
   }
 
-  @Test
-  fun `handles MethodArgumentNotValidException with no binding errors`() {
-    val result = underTest.handleException(
-      methodArgumentNotValidExceptionWithNoBindingErrors()
-    )
-
-    assertThat(
-      result,
-      isResponseEntityMatching(
-        BAD_REQUEST,
-        ErrorResponse(BAD_REQUEST, "")
-      )
-    )
-  }
-
-  @Test
-  fun `handles MethodArgumentNotValidException with field errors`() {
-    val fieldError1 = fieldError("fieldName1", "field error message1")
-    val fieldError2 = fieldError("fieldName2", "field error message2")
-
-    val result = underTest.handleException(
-      methodArgumentNotValidExceptionWithFieldErrors(fieldError1, fieldError2)
-    )
-
-    assertThat(
-      result,
-      isResponseEntityMatching(
-        BAD_REQUEST,
-        ErrorResponse(BAD_REQUEST, "fieldName1: field error message1, fieldName2: field error message2")
-      )
-    )
-  }
-
-  @Test
-  fun `handles MethodArgumentNotValidException with object errors`() {
-    val objectError1 = objectError("object error message1")
-    val objectError2 = objectError("object error message2")
-
-    val result = underTest.handleException(
-      methodArgumentNotValidExceptionWithFieldErrors(objectError1, objectError2)
-    )
-
-    assertThat(
-      result,
-      isResponseEntityMatching(
-        BAD_REQUEST,
-        ErrorResponse(BAD_REQUEST, "object error message1, object error message2")
-      )
-    )
-  }
-
   private fun isResponseEntityMatching(
     expectedHttpStatus: HttpStatus,
     expectedErrorResponse: ErrorResponse
@@ -97,21 +38,4 @@ class ManageRecallsApiExceptionHandlerTest {
       has("statusCode", { it.statusCode }, equalTo(expectedHttpStatus)),
       has("body", { it.body }, present(equalTo(expectedErrorResponse)))
     )
-
-  private fun objectError(defaultMessage: String) =
-    ObjectError(objectName, defaultMessage)
-
-  private fun fieldError(fieldName: String, defaultMessage: String) =
-    FieldError(objectName, fieldName, defaultMessage)
-
-  private fun methodArgumentNotValidExceptionWithNoBindingErrors(): MethodArgumentNotValidException =
-    methodArgumentNotValidExceptionWithFieldErrors()
-
-  private fun methodArgumentNotValidExceptionWithFieldErrors(vararg errors: ObjectError): MethodArgumentNotValidException {
-    val bindingResult: BindingResult = BeanPropertyBindingResult(object {}, objectName)
-    for (error in errors) {
-      bindingResult.addError(error)
-    }
-    return MethodArgumentNotValidException(mockk(relaxed = true), bindingResult)
-  }
 }
