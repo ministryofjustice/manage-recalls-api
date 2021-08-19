ed6b3bb8fa1ae9e5b64296b38240288b7853d58e
Mike Dunford <mike.dunford@digital.justice.gov.uk>
2021-08-19 11:45:45 +0100

    PUD-410:  Add unit test for updating a recall without all sentencing info

--- src/main/kotlin/uk/gov/justice/digital/hmpps/managerecallsapi/controller/SearchController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/managerecallsapi/controller/SearchController.kt
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.security.access.prepost.PreAuthorize
-import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -18,7 +17,6 @@ import java.time.LocalDate
 @RestController
 @RequestMapping(produces = [MediaType.APPLICATION_JSON_VALUE])
 @PreAuthorize("hasRole('ROLE_MANAGE_RECALLS')")
-@Validated
 class SearchController(@Autowired private val prisonerOffenderSearchClient: PrisonerOffenderSearchClient) {
 
   @PostMapping("/search")
