65a935710cf8c20f41e4ace3d8e0ada6ee8b76ac
Natalia Oskina <natalia.oskina@equalexperts.com>
2021-09-01 14:10:06 +0100

    PUD-439: Add tests for prison validation check

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/managerecallsapi/component/PrisonRegisterTest.kt
@@ -0,0 +1,31 @@
+package uk.gov.justice.digital.hmpps.managerecallsapi.component
+
+import com.natpryce.hamkrest.assertion.assertThat
+import com.natpryce.hamkrest.equalTo
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.jupiter.api.Test
+import reactor.core.publisher.Mono
+import uk.gov.justice.digital.hmpps.managerecallsapi.controller.PrisonValidationService
+import uk.gov.justice.digital.hmpps.managerecallsapi.prisonData.PrisonRegisterClient
+
+class PrisonRegisterTest : ComponentTestBase() {
+
+  private val prisonRegister = mockk<PrisonRegisterClient>()
+  private val prisonValidationService = PrisonValidationService(prisonRegister)
+
+  @Test
+  fun `can check prison register`() {
+    val prisonList: MutableList<PrisonRegisterClient.Prison> = ArrayList()
+    prisonList.add(PrisonRegisterClient.Prison("HPI"))
+    prisonList.add(PrisonRegisterClient.Prison("MWI"))
+    prisonList.add(PrisonRegisterClient.Prison("ALI"))
+    prisonList.add(PrisonRegisterClient.Prison("FYI"))
+
+    every { prisonRegister.getAllPrisons() } returns Mono.just(prisonList)
+
+    val prisonValidationResult = prisonValidationService.isValidPrison("MWI")
+
+    assertThat(prisonValidationResult, equalTo(true))
+  }
+}
