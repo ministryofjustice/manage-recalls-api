77866e33150ec3230c53979a4747577739b5f2d9
Phil Segal <phil@phisec.co.uk>
2021-09-06 14:23:58 +0100

    PUD-104: Add BOOKED_ON status for recalls with uploaded docs (last in the booking flow)

--- src/test/kotlin/uk/gov/justice/digital/hmpps/managerecallsapi/controller/RecallResponseTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/managerecallsapi/controller/RecallResponseTest.kt
@@ -3,6 +3,7 @@ package uk.gov.justice.digital.hmpps.managerecallsapi.controller
 import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
 import org.junit.jupiter.api.Test
+import uk.gov.justice.digital.hmpps.managerecallsapi.db.RecallDocumentCategory
 import uk.gov.justice.digital.hmpps.managerecallsapi.domain.NomsNumber
 import uk.gov.justice.digital.hmpps.managerecallsapi.domain.RecallId
 import java.time.OffsetDateTime
@@ -11,16 +12,44 @@ import java.util.UUID
 class RecallResponseTest {
 
   @Test
+  fun `RecallResponse without recallNotificationEmailSentDateTime or documents set returns null status`() {
+    val recall = RecallResponse(RecallId(UUID.randomUUID()), NomsNumber("A12345AA"))
+
+    assertThat(recall.status, equalTo(null))
+  }
+
+  @Test
+  fun `RecallResponse with at least 1 document and without recallNotificationEmailSentDateTime set returns status BOOKED_ON`() {
+    val recall = RecallResponse(
+      RecallId(UUID.randomUUID()), NomsNumber("A12345AA"),
+      documents = listOf(
+        ApiRecallDocument(UUID.randomUUID(), RecallDocumentCategory.LICENCE, "blah")
+      )
+    )
+    assertThat(recall.status, equalTo(Status.BOOKED_ON))
+  }
+
+  @Test
   fun `RecallResponse with recallNotificationEmailSentDateTime set returns status RECALL_NOTIFICATION_ISSUED`() {
-    val recall = RecallResponse(RecallId(UUID.randomUUID()), NomsNumber("A12345AA"), recallNotificationEmailSentDateTime = OffsetDateTime.now())
+    val recall = RecallResponse(
+      RecallId(UUID.randomUUID()),
+      NomsNumber("A12345AA"),
+      recallNotificationEmailSentDateTime = OffsetDateTime.now()
+    )
 
     assertThat(recall.status, equalTo(Status.RECALL_NOTIFICATION_ISSUED))
   }
 
   @Test
-  fun `RecallResponse without recallNotificationEmailSentDateTime set returns null status`() {
-    val recall = RecallResponse(RecallId(UUID.randomUUID()), NomsNumber("A12345AA"))
+  fun `RecallResponse with recallNotificationEmailSentDateTime set and at least 1 document returns status RECALL_NOTIFICATION_ISSUED`() {
+    val recall = RecallResponse(
+      RecallId(UUID.randomUUID()), NomsNumber("A12345AA"),
+      documents = listOf(
+        ApiRecallDocument(UUID.randomUUID(), RecallDocumentCategory.LICENCE, "blah")
+      ),
+      recallNotificationEmailSentDateTime = OffsetDateTime.now()
+    )
 
-    assertThat(recall.status, equalTo(null))
+    assertThat(recall.status, equalTo(Status.RECALL_NOTIFICATION_ISSUED))
   }
 }
