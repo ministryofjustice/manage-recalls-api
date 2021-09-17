359ef2ac1571f01b0df2b0736b208d4d641c127b
Mike Dunford <mike.dunford@digital.justice.gov.uk>
2021-09-17 14:09:33 +0100

    PUD-540:  Add UserDetails table and repository

--- src/main/kotlin/uk/gov/justice/digital/hmpps/managerecallsapi/db/Recall.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/managerecallsapi/db/Recall.kt
@@ -11,7 +11,6 @@ import uk.gov.justice.digital.hmpps.managerecallsapi.domain.RecallId
 import java.time.LocalDate
 import java.time.OffsetDateTime
 import java.util.UUID
-import javax.persistence.AttributeConverter
 import javax.persistence.CascadeType.ALL
 import javax.persistence.Column
 import javax.persistence.Convert
@@ -143,15 +142,6 @@ data class Recall(
     )
 
   fun recallId() = RecallId(id)
-}
-
-class NomsNumberJpaConverter : CustomJpaConverter<NomsNumber, String>({ it.value }, ::NomsNumber)
-
-abstract class CustomJpaConverter<IN, OUT>(private val toDbFn: (IN) -> OUT, private val fromDbFn: (OUT) -> IN) :
-  AttributeConverter<IN, OUT> {
-  override fun convertToDatabaseColumn(value: IN): OUT = toDbFn(value)
-
-  override fun convertToEntityAttribute(value: OUT): IN = fromDbFn(value)
 }
 
 @Embeddable
