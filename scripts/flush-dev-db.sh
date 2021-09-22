63b7469458286055b9e7d1e7dba381b3fa27e2a2
Darren Oakley <darren.oakley@digital.justice.gov.uk>
2021-09-22 14:10:21 +0100

    PUD-657 Clear down the S3 bucket as well as the database.

--- scripts/flush-dev-db.sh
+++ scripts/flush-dev-db.sh
@@ -14,13 +14,15 @@ check_dep "jq" "brew install jq"
 check_dep "aws" "brew install awscli"
 check_dep "kubectl" "asdf install kubectl 1.19.15"
 
-# check environment
+# check for the ENV variable
 set +u
 if [[ -z "${ENV}" ]]; then
-  echo "You must set the ENV environment variable - allowed values: [dev, preprod, prod]"
+  echo "You must set the ENV K8S_NAMESPACE variable - allowed values: [dev, preprod, prod]"
   exit 1
 fi
 set -u
+
+K8S_NAMESPACE="ppud-replacement-${ENV}"
 
 # kick off port forward
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
@@ -38,11 +40,10 @@ done
 ##
 
 # db connection details
-ENVIRONMENT="ppud-replacement-${ENV}"
-SECRET=manage-recalls-database
-DB_NAME=$(kubectl -n "${ENVIRONMENT}" get secret "${SECRET}" -o json | jq -r '.data.name | @base64d')
-DB_USER=$(kubectl -n "${ENVIRONMENT}" get secret "${SECRET}" -o json | jq -r '.data.username | @base64d')
-DB_PASS=$(kubectl -n "${ENVIRONMENT}" get secret "${SECRET}" -o json | jq -r '.data.password | @base64d')
+DB_SECRET=manage-recalls-database
+DB_NAME=$(kubectl -n "${K8S_NAMESPACE}" get secret "${DB_SECRET}" -o json | jq -r '.data.name | @base64d')
+DB_USER=$(kubectl -n "${K8S_NAMESPACE}" get secret "${DB_SECRET}" -o json | jq -r '.data.username | @base64d')
+DB_PASS=$(kubectl -n "${K8S_NAMESPACE}" get secret "${DB_SECRET}" -o json | jq -r '.data.password | @base64d')
 DB_PORT=5432
 
 # run the things
@@ -54,9 +55,26 @@ function run_psql_cmd() {
     --port "${DB_PORT}" \
     --dbname "${DB_NAME}" \
     --command "${1}"
-
-  echo "done."
 }
 
-echo "Clearing recall table"
-run_psql_cmd "TRUNCATE recall CASCADE"
+echo "Clearing the database..."
+# run_psql_cmd "TRUNCATE recall CASCADE"
+echo "done."
+
+##
+## Clear the S3 bucket
+##
+
+# connection details
+S3_SECRET=manage-recalls-s3-bucket
+AWS_ACCESS_KEY_ID=$(kubectl -n "${K8S_NAMESPACE}" get secret "${S3_SECRET}" -o json | jq -r '.data.access_key_id | @base64d')
+AWS_SECRET_ACCESS_KEY=$(kubectl -n "${K8S_NAMESPACE}" get secret "${S3_SECRET}" -o json | jq -r '.data.secret_access_key | @base64d')
+AWS_BUCKET=$(kubectl -n "${K8S_NAMESPACE}" get secret "${S3_SECRET}" -o json | jq -r '.data.bucket_name | @base64d')
+AWS_DEFAULT_REGION="eu-west-2"
+export AWS_ACCESS_KEY_ID
+export AWS_SECRET_ACCESS_KEY
+export AWS_DEFAULT_REGION
+
+echo "Emptying the S3 bucket..."
+aws s3 rm "s3://${AWS_BUCKET}" --recursive
+echo "done."
--- scripts/port-forward-db.sh
+++ scripts/port-forward-db.sh
@@ -12,33 +12,33 @@ function check_dep {
 check_dep "psql" "brew install postgresql"
 check_dep "jq" "brew install jq"
 
-# check environment
+# check for the ENV variable
 set +u
 if [[ -z "${ENV}" ]]; then
-  echo "You must set the ENV environment variable - allowed values: [dev, preprod, prod]"
+  echo "You must set the ENV K8S_NAMESPACE variable - allowed values: [dev, preprod, prod]"
   exit 1
 fi
 set -u
 
-ENVIRONMENT="ppud-replacement-${ENV}"
+K8S_NAMESPACE="ppud-replacement-${ENV}"
 PFP_NAME="manage-recalls-api-port-forward-$(whoami | tr ._ -)"
 SECRET=manage-recalls-database
 
-DB_HOST=$(kubectl -n "${ENVIRONMENT}" get secret "${SECRET}" -o json | jq -r '.data.host | @base64d')
-DB_USER=$(kubectl -n "${ENVIRONMENT}" get secret "${SECRET}" -o json | jq -r '.data.username | @base64d')
-DB_PASS=$(kubectl -n "${ENVIRONMENT}" get secret "${SECRET}" -o json | jq -r '.data.password | @base64d')
+DB_HOST=$(kubectl -n "${K8S_NAMESPACE}" get secret "${SECRET}" -o json | jq -r '.data.host | @base64d')
+DB_USER=$(kubectl -n "${K8S_NAMESPACE}" get secret "${SECRET}" -o json | jq -r '.data.username | @base64d')
+DB_PASS=$(kubectl -n "${K8S_NAMESPACE}" get secret "${SECRET}" -o json | jq -r '.data.password | @base64d')
 DB_PORT=5432
 
-echo "Connecting to ${ENVIRONMENT}"
+echo "Connecting to ${K8S_NAMESPACE}"
 
 set +e
-PFP_COUNT=$(kubectl -n "${ENVIRONMENT}" get pods | grep -c "${PFP_NAME}")
+PFP_COUNT=$(kubectl -n "${K8S_NAMESPACE}" get pods | grep -c "${PFP_NAME}")
 set -e
 
 if [ "${PFP_COUNT}" -eq "0" ]; then
   echo "Starting up port forward pod..."
 
-  kubectl -n "${ENVIRONMENT}" run \
+  kubectl -n "${K8S_NAMESPACE}" run \
     "${PFP_NAME}" \
     --image=ministryofjustice/port-forward \
     --port="${DB_PORT}" \
@@ -50,7 +50,7 @@ if [ "${PFP_COUNT}" -eq "0" ]; then
 fi
 
 set +e
-cmd="kubectl -n ${ENVIRONMENT} port-forward ${PFP_NAME} ${DB_PORT}:${DB_PORT}"
+cmd="kubectl -n ${K8S_NAMESPACE} port-forward ${PFP_NAME} ${DB_PORT}:${DB_PORT}"
 pid=$(pgrep -f "${cmd}")
 set -e
 
