03c704e379886108a08e0b3aa3f4f05fd33bc2ee
Darren Oakley <darren.oakley@digital.justice.gov.uk>
2021-09-22 13:16:30 +0100

    Make the flush script more robust...
    * Check the required system dependencies are met
    * Use `pg_isready` to check for the postgres connection

--- scripts/flush-dev-db.sh
+++ scripts/flush-dev-db.sh
@@ -1,14 +1,44 @@
 #!/bin/bash
-
 set -euo pipefail
+
+function check_dep {
+  if ! command -v "${1}" &>/dev/null; then
+    echo "You need '${1}' - '${2}'"
+    exit 1
+  fi
+}
+
+# Check dependencies
+check_dep "psql" "brew install postgresql"
+check_dep "jq" "brew install jq"
+check_dep "aws" "brew install awscli"
+check_dep "kubectl" "asdf install kubectl 1.19.15"
+
+# check environment
+set +u
+if [[ -z "${ENV}" ]]; then
+  echo "You must set the ENV environment variable - allowed values: [dev, preprod, prod]"
+  exit 1
+fi
+set -u
 
 # kick off port forward
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
-bash "${SCRIPT_DIR}/port-forward-db.sh" &
-sleep 5
+# shellcheck disable=SC1091
+. "${SCRIPT_DIR}/port-forward-db.sh" &
+sleep 2
+
+until pg_isready -q -h "127.0.0.1" -p 5432; do
+  echo "Postgres is unavailable - waiting..."
+  sleep 1
+done
+
+##
+## Clear the database...
+##
 
 # db connection details
-ENVIRONMENT="ppud-replacement-dev"
+ENVIRONMENT="ppud-replacement-${ENV}"
 SECRET=manage-recalls-database
 DB_NAME=$(kubectl -n "${ENVIRONMENT}" get secret "${SECRET}" -o json | jq -r '.data.name | @base64d')
 DB_USER=$(kubectl -n "${ENVIRONMENT}" get secret "${SECRET}" -o json | jq -r '.data.username | @base64d')
