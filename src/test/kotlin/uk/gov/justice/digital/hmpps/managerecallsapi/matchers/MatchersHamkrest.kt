32c5870e770837ab58a47d2ddd35294b8e43a3c1
Mike Dunford <mike.dunford@digital.justice.gov.uk>
2021-09-22 16:54:00 +0100

    PUD-644:  Turn on debug logging for gotenberg container.  Tweak to RealPdfDocumentGeneratorTest

--- src/test/kotlin/uk/gov/justice/digital/hmpps/managerecallsapi/matchers/MatchersHamkrest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/managerecallsapi/matchers/MatchersHamkrest.kt
@@ -1,11 +1,15 @@
 package uk.gov.justice.digital.hmpps.managerecallsapi.matchers
 
+import com.lowagie.text.pdf.PdfReader
 import com.natpryce.hamkrest.MatchResult
 import com.natpryce.hamkrest.Matcher
 import com.natpryce.hamkrest.allOf
 import com.natpryce.hamkrest.cast
 import com.natpryce.hamkrest.equalTo
+import com.natpryce.hamkrest.has
 import com.natpryce.hamkrest.hasSize
+import uk.gov.justice.digital.hmpps.managerecallsapi.controller.Pdf
+import java.util.Base64
 
 fun <T> isSingleItemMatching(matcher: Matcher<T>): Matcher<Collection<T>> =
   allOf(
@@ -18,3 +22,20 @@ fun <T> isSingleItemMatching(matcher: Matcher<T>): Matcher<Collection<T>> =
       }
     }
   )
+
+fun hasNumberOfPages(numberOfPagesMatcher: Matcher<Int>): Matcher<Pdf> =
+  has(
+    "number of pages",
+    { Base64.getDecoder().decode(it.content.toByteArray()) },
+    isPdfWithNumberOfPages(numberOfPagesMatcher)
+  )
+
+fun isPdfWithNumberOfPages(numberOfPagesMatcher: Matcher<Int>): Matcher<ByteArray> =
+  object : Matcher<ByteArray> {
+    override val description: String = numberOfPagesMatcher.description
+
+    override fun invoke(actual: ByteArray): MatchResult =
+      PdfReader(actual).use { pdfReader ->
+        numberOfPagesMatcher(pdfReader.numberOfPages)
+      }
+  }
