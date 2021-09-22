528f3273a2abeb88a831c757bd110404cef9586c
Darren Oakley <darren.oakley@digital.justice.gov.uk>
2021-09-22 13:15:48 +0100

    Update the port forward script
    If there is already a port forward in place, re-use it - don't crash...

--- scripts/port-forward-db.sh
+++ scripts/port-forward-db.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
-
 set -euo pipefail
 
-ENVIRONMENT="ppud-replacement-${ENV:-dev}"
+function check_dep {
+  if ! command -v "${1}" &>/dev/null; then
+    echo "You need '${1}' - '${2}'"
+    exit 1
+  fi
+}
+
+# Check dependencies
+check_dep "psql" "brew install postgresql"
+check_dep "jq" "brew install jq"
+
+# check environment
+set +u
+if [[ -z "${ENV}" ]]; then
+  echo "You must set the ENV environment variable - allowed values: [dev, preprod, prod]"
+  exit 1
+fi
+set -u
+
+ENVIRONMENT="ppud-replacement-${ENV}"
 PFP_NAME="manage-recalls-api-port-forward-$(whoami | tr ._ -)"
 SECRET=manage-recalls-database
 
@@ -31,6 +49,14 @@ if [ "${PFP_COUNT}" -eq "0" ]; then
   sleep 5
 fi
 
+set +e
+cmd="kubectl -n ${ENVIRONMENT} port-forward ${PFP_NAME} ${DB_PORT}:${DB_PORT}"
+pid=$(pgrep -f "${cmd}")
+set -e
+
 echo "You can now connect to the database on 127.0.0.1:${DB_PORT} - username: ${DB_USER} - password: ${DB_PASS}"
 
-kubectl -n "${ENVIRONMENT}" port-forward "${PFP_NAME}" "${DB_PORT}:${DB_PORT}"
+# if the port-forward isn't already running, start it...
+if [[ -z "${pid}" ]]; then
+  ${cmd}
+fi
