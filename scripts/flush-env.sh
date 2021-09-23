9412f1ed5312e561a5261c4f4953b16e2036b223
Darren Oakley <darren.oakley@digital.justice.gov.uk>
2021-09-23 16:14:11 +0100

    Use a HTTP proxy to tunnel AWS CLI calls via the cluster
    This gets us around the restrictions on where the S3 bucket can be
    accessed from...
    
    Tested and working on clearing down pre-prod.

--- scripts/flush-env.sh
+++ scripts/flush-env.sh
@@ -19,10 +19,11 @@ function check_dep {
 }
 
 # Check dependencies
-check_dep "psql" "brew install postgresql"
-check_dep "jq" "brew install jq"
 check_dep "aws" "brew install awscli"
+check_dep "jq" "brew install jq"
 check_dep "kubectl" "asdf install kubectl 1.19.15"
+check_dep "netcat" "brew install netcat"
+check_dep "psql" "brew install postgresql"
 
 # get cli options
 while getopts :e:h opt; do
@@ -56,15 +57,15 @@ fi
 set -u
 
 K8S_NAMESPACE="ppud-replacement-${ENV}"
-
-# kick off port forward
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+
+# kick off db port forward
 # shellcheck disable=SC1091
 ${SCRIPT_DIR}/port-forward-db.sh -e ${ENV} &
 sleep 2
 
 until pg_isready -q -h "127.0.0.1" -p 5432; do
-  echo "Postgres is unavailable - waiting..."
+  echo "DB proxy is unavailable - waiting..."
   sleep 1
 done
 
@@ -98,16 +99,35 @@ echo "done."
 ## Clear the S3 bucket
 ##
 
+# kick off http proxy port forward
+# shellcheck disable=SC1091
+${SCRIPT_DIR}/port-forward-http-proxy.sh -e ${ENV} &
+sleep 2
+
+until netcat -z 127.0.0.1 3128; do
+  echo "HTTP proxy is unavailable - waiting..."
+  sleep 1
+done
+
 # connection details
 S3_SECRET=manage-recalls-s3-bucket
 AWS_ACCESS_KEY_ID=$(kubectl -n "${K8S_NAMESPACE}" get secret "${S3_SECRET}" -o json | jq -r '.data.access_key_id | @base64d')
 AWS_SECRET_ACCESS_KEY=$(kubectl -n "${K8S_NAMESPACE}" get secret "${S3_SECRET}" -o json | jq -r '.data.secret_access_key | @base64d')
 AWS_BUCKET=$(kubectl -n "${K8S_NAMESPACE}" get secret "${S3_SECRET}" -o json | jq -r '.data.bucket_name | @base64d')
-AWS_DEFAULT_REGION="eu-west-2"
 export AWS_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY
-export AWS_DEFAULT_REGION
+export AWS_DEFAULT_REGION="eu-west-2"
+export HTTP_PROXY=http://127.0.0.1:3128
+export HTTPS_PROXY=http://127.0.0.1:3128
 
 echo "Emptying the S3 bucket..."
 aws s3 rm "s3://${AWS_BUCKET}" --recursive
+echo "done."
+
+##
+## Tidy up
+##
+
+echo "Closing proxies..."
+pkill kubectl
 echo "done."
