6be1cf23f46bb39d90f21aba2c0574d1c1d2a32c
Phil Segal <philsegal@users.noreply.github.com>
2021-09-24 14:18:24 +0100

    PUD-468: Fix flyway scripts (#197)

--- src/main/resources/db/migration/V40__Migrate_revocation_order_to_recall_documents.sql
+++ src/main/resources/db/migration/V40__Migrate_revocation_order_to_recall_documents.sql
@@ -1,3 +1,1 @@
-INSERT INTO recall_document SELECT revocation_order_id, ID, 'REVOCATION_ORDER', null FROM RECALL;
-
-ALTER TABLE recall DROP COLUMN revocation_order_id;
+INSERT INTO recall_document SELECT revocation_order_id, ID, 'REVOCATION_ORDER', null FROM RECALL where revocation_order_id is not null;
