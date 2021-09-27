a0cac94a58521b5002d79ae3316bff9e0d3ef737
Mike Dunford <mike.dunford@digital.justice.gov.uk>
2021-09-27 10:54:44 +0100

    PUD-644:  Add DossierGenerationGotenbergComponentTest to test generating a dossier using real gotenberg instead of mock server

--- src/main/kotlin/uk/gov/justice/digital/hmpps/managerecallsapi/documents/GotenbergApi.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/managerecallsapi/documents/GotenbergApi.kt
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Component
 import org.springframework.util.MultiValueMap
 import org.springframework.web.reactive.function.client.WebClient
 import reactor.core.publisher.Mono
+import reactor.netty.http.client.HttpClientRequest
+import java.time.Duration.ofSeconds
 
 @Component
 class GotenbergApi(
@@ -25,6 +27,7 @@ class GotenbergApi(
   ) = webClient
     .post()
     .uri("$gotenbergEndpointUrl$path")
+    .httpRequest { it.getNativeRequest<HttpClientRequest>().responseTimeout(ofSeconds(10)) }
     .contentType(MULTIPART_FORM_DATA)
     .bodyValue(documentBody)
     .retrieve()
