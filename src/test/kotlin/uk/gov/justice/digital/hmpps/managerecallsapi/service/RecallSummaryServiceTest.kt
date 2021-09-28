3d62276f2574f2ba30718883096925ac9c99eaaf
Mike Dunford <mike.dunford@digital.justice.gov.uk>
2021-09-28 13:19:05 +0100

    PUD-151:  Add RecallSummaryContextFactory to simplify RecallSummaryService

--- src/test/kotlin/uk/gov/justice/digital/hmpps/managerecallsapi/service/RecallSummaryServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/managerecallsapi/service/RecallSummaryServiceTest.kt
@@ -1,63 +1,50 @@
 package uk.gov.justice.digital.hmpps.managerecallsapi.service
 
+import com.natpryce.hamkrest.assertion.assertThat
+import com.natpryce.hamkrest.equalTo
 import io.mockk.every
 import io.mockk.mockk
+import io.mockk.slot
 import org.junit.jupiter.api.Assertions.assertArrayEquals
 import org.junit.jupiter.api.Test
 import org.springframework.core.io.ClassPathResource
 import reactor.core.publisher.Mono
-import uk.gov.justice.digital.hmpps.managerecallsapi.controller.PrisonLookupService
-import uk.gov.justice.digital.hmpps.managerecallsapi.controller.SearchRequest
-import uk.gov.justice.digital.hmpps.managerecallsapi.db.Recall
-import uk.gov.justice.digital.hmpps.managerecallsapi.db.RecallRepository
 import uk.gov.justice.digital.hmpps.managerecallsapi.documents.ImageData.Companion.recallImage
 import uk.gov.justice.digital.hmpps.managerecallsapi.documents.PdfDocumentGenerationService
-import uk.gov.justice.digital.hmpps.managerecallsapi.domain.NomsNumber
 import uk.gov.justice.digital.hmpps.managerecallsapi.domain.RecallId
 import uk.gov.justice.digital.hmpps.managerecallsapi.domain.random
-import uk.gov.justice.digital.hmpps.managerecallsapi.search.Prisoner
-import uk.gov.justice.digital.hmpps.managerecallsapi.search.PrisonerOffenderSearchClient
 import uk.gov.justice.digital.hmpps.managerecallsapi.service.RecallImage.HmppsLogo
 
 class RecallSummaryServiceTest {
   private val pdfDocumentGenerationService = mockk<PdfDocumentGenerationService>()
   private val recallSummaryGenerator = mockk<RecallSummaryGenerator>()
-  private val recallRepository = mockk<RecallRepository>()
-  private val prisonLookupService = mockk<PrisonLookupService>()
-  private val prisonerOffenderSearchClient = mockk<PrisonerOffenderSearchClient>()
+  private val recallSummaryContextFactory = mockk<RecallSummaryContextFactory>()
 
   private val underTest = RecallSummaryService(
     pdfDocumentGenerationService,
-    recallSummaryGenerator, recallRepository, prisonLookupService, prisonerOffenderSearchClient
+    recallSummaryGenerator,
+    recallSummaryContextFactory
   )
 
   @Test
   fun `generates the recall summary PDF with required information`() {
     val recallId = ::RecallId.random()
-    val nomsNumber = NomsNumber("nomsNumber")
-    val currentPrison = "AAA"
-    val lastReleasePrison = "ZZZ"
-    val prisoner = mockk<Prisoner>()
-    val recall = Recall(recallId, nomsNumber, currentPrison = currentPrison, lastReleasePrison = lastReleasePrison)
-    val currentPrisonName = "Current Prison Name"
-    val lastReleasePrisonName = "Last Release Prison Name"
     val recallSummaryHtmlWithoutPageCount = "generated Html without page count"
     val recallSummaryHtmlWithPageCount = "generated Html with page count"
     val pdfWith3Pages = ClassPathResource("/document/3_pages_unnumbered.pdf").file.readBytes()
-    val recallNotificationTotalNumberOfPages = 5
 
-    every { recallRepository.getByRecallId(recallId) } returns recall
-    every { prisonLookupService.getPrisonName(currentPrison) } returns currentPrisonName
-    every { prisonLookupService.getPrisonName(lastReleasePrison) } returns lastReleasePrisonName
-    every { prisonerOffenderSearchClient.prisonerSearch(SearchRequest(nomsNumber)) } returns Mono.just(listOf(prisoner))
+    val recallSummaryContext = RecallSummaryContext(mockk(), mockk(), "don't care", "don't care")
+    val recallSummaryContextWithPageCountSlot = slot<RecallSummaryContext>()
 
-    every { recallSummaryGenerator.generateHtml(RecallSummaryContext(recall, prisoner, lastReleasePrisonName, currentPrisonName)) } returns recallSummaryHtmlWithoutPageCount
+    every { recallSummaryContextFactory.createRecallSummaryContext(recallId) } returns Mono.just(recallSummaryContext)
+    every { recallSummaryGenerator.generateHtml(recallSummaryContext) } returns recallSummaryHtmlWithoutPageCount
     every { pdfDocumentGenerationService.generatePdf(recallSummaryHtmlWithoutPageCount, recallImage(HmppsLogo)) } returns Mono.just(pdfWith3Pages)
-    every { recallSummaryGenerator.generateHtml(RecallSummaryContext(recall, prisoner, lastReleasePrisonName, currentPrisonName, recallNotificationTotalNumberOfPages)) } returns recallSummaryHtmlWithPageCount
+    every { recallSummaryGenerator.generateHtml(capture(recallSummaryContextWithPageCountSlot)) } returns recallSummaryHtmlWithPageCount
     every { pdfDocumentGenerationService.generatePdf(recallSummaryHtmlWithPageCount, recallImage(HmppsLogo)) } returns Mono.just(pdfWith3Pages)
 
     val result = underTest.getPdf(recallId).block()!!
 
+    assertThat(recallSummaryContextWithPageCountSlot.captured.recallNotificationTotalNumberOfPages, equalTo(5))
     assertArrayEquals(pdfWith3Pages, result)
   }
 }
