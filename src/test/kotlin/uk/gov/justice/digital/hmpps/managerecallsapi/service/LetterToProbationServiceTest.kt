358d1f5e0b415bd39b7e0da8929871515f59964d
Mike Dunford <mike.dunford@digital.justice.gov.uk>
2021-10-01 14:49:03 +0100

    PUD-399:  Rename methods for consistency.  Add missing test

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/managerecallsapi/service/LetterToProbationServiceTest.kt
@@ -0,0 +1,38 @@
+package uk.gov.justice.digital.hmpps.managerecallsapi.service
+
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.jupiter.api.Assertions.assertArrayEquals
+import org.junit.jupiter.api.Test
+import reactor.core.publisher.Mono
+import uk.gov.justice.digital.hmpps.managerecallsapi.documents.ImageData.Companion.recallImage
+import uk.gov.justice.digital.hmpps.managerecallsapi.documents.PdfDocumentGenerationService
+import uk.gov.justice.digital.hmpps.managerecallsapi.service.RecallImage.HmppsLogo
+
+class LetterToProbationServiceTest {
+  private val pdfDocumentGenerationService = mockk<PdfDocumentGenerationService>()
+  private val letterToProbationGenerator = mockk<LetterToProbationGenerator>()
+  private val letterToProbationContextFactory = mockk<LetterToProbationContextFactory>()
+
+  private val underTest = LetterToProbationService(
+    letterToProbationContextFactory,
+    letterToProbationGenerator,
+    pdfDocumentGenerationService
+  )
+
+  @Test
+  fun `create letter to probation generates html and creates the PDF`() {
+    val recallNotificationContext = mockk<RecallNotificationContext>()
+    val letterToProbationContext = mockk<LetterToProbationContext>()
+    val generatedHtml = "some html"
+    val pdfBytes = "pdf".toByteArray()
+
+    every { letterToProbationContextFactory.createContext(recallNotificationContext) } returns letterToProbationContext
+    every { letterToProbationGenerator.generateHtml(letterToProbationContext) } returns generatedHtml
+    every { pdfDocumentGenerationService.generatePdf(generatedHtml, recallImage(HmppsLogo)) } returns Mono.just(pdfBytes)
+
+    val result = underTest.createPdf(recallNotificationContext).block()!!
+
+    assertArrayEquals(result, pdfBytes)
+  }
+}
