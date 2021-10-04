6f9b09d7aeab11db332517fce79ca365ece14d6c
Natalia Oskina <natalia.oskina@equalexperts.com>
2021-10-04 17:25:45 +0100

    PUD-731: Add dossier context factory

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/managerecallsapi/service/DossierContextFactory.kt
@@ -0,0 +1,32 @@
+package uk.gov.justice.digital.hmpps.managerecallsapi.service
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+import uk.gov.justice.digital.hmpps.managerecallsapi.controller.PrisonLookupService
+import uk.gov.justice.digital.hmpps.managerecallsapi.controller.SearchRequest
+import uk.gov.justice.digital.hmpps.managerecallsapi.db.Recall
+import uk.gov.justice.digital.hmpps.managerecallsapi.db.RecallRepository
+import uk.gov.justice.digital.hmpps.managerecallsapi.domain.PrisonName
+import uk.gov.justice.digital.hmpps.managerecallsapi.domain.RecallId
+import uk.gov.justice.digital.hmpps.managerecallsapi.search.Prisoner
+import uk.gov.justice.digital.hmpps.managerecallsapi.search.PrisonerOffenderSearchClient
+
+@Component
+class DossierContextFactory(
+  @Autowired private val recallRepository: RecallRepository,
+  @Autowired private val prisonLookupService: PrisonLookupService,
+  @Autowired private val prisonerOffenderSearchClient: PrisonerOffenderSearchClient
+) {
+  fun createContext(recallId: RecallId): DossierContext {
+    val recall = recallRepository.getByRecallId(recallId)
+    val prisoner = prisonerOffenderSearchClient.prisonerSearch(SearchRequest(recall.nomsNumber)).block()!!.first()
+    val currentPrisonName = prisonLookupService.getPrisonName(recall.currentPrison!!)
+    return DossierContext(recall, prisoner, currentPrisonName)
+  }
+}
+
+data class DossierContext(
+  val recall: Recall,
+  val prisoner: Prisoner,
+  val currentPrisonName: PrisonName,
+)
--- src/main/kotlin/uk/gov/justice/digital/hmpps/managerecallsapi/service/ReasonsForRecallService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/managerecallsapi/service/ReasonsForRecallService.kt
@@ -3,31 +3,24 @@ package uk.gov.justice.digital.hmpps.managerecallsapi.service
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Mono
-import uk.gov.justice.digital.hmpps.managerecallsapi.controller.SearchRequest
 import uk.gov.justice.digital.hmpps.managerecallsapi.db.Recall
-import uk.gov.justice.digital.hmpps.managerecallsapi.db.RecallRepository
 import uk.gov.justice.digital.hmpps.managerecallsapi.documents.PdfDocumentGenerationService
-import uk.gov.justice.digital.hmpps.managerecallsapi.domain.RecallId
 import uk.gov.justice.digital.hmpps.managerecallsapi.search.Prisoner
-import uk.gov.justice.digital.hmpps.managerecallsapi.search.PrisonerOffenderSearchClient
 
 @Service
 class ReasonsForRecallService(
   @Autowired private val pdfDocumentGenerationService: PdfDocumentGenerationService,
   @Autowired private val reasonsForRecallGenerator: ReasonsForRecallGenerator,
-  @Autowired private val recallRepository: RecallRepository,
-  @Autowired private val prisonerOffenderSearchClient: PrisonerOffenderSearchClient,
 ) {
 
-  fun getPdf(recallId: RecallId): Mono<ByteArray> {
-    val recall = recallRepository.getByRecallId(recallId)
-
-    return prisonerOffenderSearchClient.prisonerSearch(SearchRequest(recall.nomsNumber))
-      .flatMap { prisoners ->
-        val html = reasonsForRecallGenerator.generateHtml(ReasonsForRecallContext(recall, prisoners.first()))
-        pdfDocumentGenerationService.generatePdf(html)
-      }
-  }
+  fun getPdf(dossierContext: DossierContext): Mono<ByteArray> =
+    pdfDocumentGenerationService.generatePdf(
+      reasonsForRecallGenerator.generateHtml(
+        ReasonsForRecallContext(
+          dossierContext.recall, dossierContext.prisoner
+        )
+      )
+    )
 }
 
 data class ReasonsForRecallContext(val recall: Recall, val prisoner: Prisoner)
