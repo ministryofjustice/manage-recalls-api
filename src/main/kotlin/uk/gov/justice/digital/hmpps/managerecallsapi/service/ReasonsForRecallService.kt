c26b6b76a994510b265306d564c626eea718c6ff
Mike Dunford <mike.dunford@digital.justice.gov.uk>
2021-10-05 09:25:55 +0100

    PUD-731: Add missing mandatory fields to CreateDossierComponentTest

--- src/main/kotlin/uk/gov/justice/digital/hmpps/managerecallsapi/service/ReasonsForRecallService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/managerecallsapi/service/ReasonsForRecallService.kt
@@ -3,24 +3,15 @@ package uk.gov.justice.digital.hmpps.managerecallsapi.service
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Mono
-import uk.gov.justice.digital.hmpps.managerecallsapi.db.Recall
 import uk.gov.justice.digital.hmpps.managerecallsapi.documents.PdfDocumentGenerationService
-import uk.gov.justice.digital.hmpps.managerecallsapi.search.Prisoner
 
 @Service
 class ReasonsForRecallService(
-  @Autowired private val pdfDocumentGenerationService: PdfDocumentGenerationService,
   @Autowired private val reasonsForRecallGenerator: ReasonsForRecallGenerator,
+  @Autowired private val pdfDocumentGenerationService: PdfDocumentGenerationService,
 ) {
-
   fun createPdf(dossierContext: DossierContext): Mono<ByteArray> =
     pdfDocumentGenerationService.generatePdf(
-      reasonsForRecallGenerator.generateHtml(
-        ReasonsForRecallContext(
-          dossierContext.recall, dossierContext.prisoner
-        )
-      )
+      reasonsForRecallGenerator.generateHtml(dossierContext.getReasonsForRecallContext())
     )
 }
-
-data class ReasonsForRecallContext(val recall: Recall, val prisoner: Prisoner)
