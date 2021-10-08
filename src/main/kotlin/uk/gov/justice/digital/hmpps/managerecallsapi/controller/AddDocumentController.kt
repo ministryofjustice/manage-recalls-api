c4a0ebc994ee2f8ba7051729d7211188bfa69a38
Mike Dunford <mike.dunford@digital.justice.gov.uk>
2021-10-08 10:18:20 +0100

    PUD-682: Return 404 instead of 400 if a recal is not found when uploading a document for that recall

--- src/main/kotlin/uk/gov/justice/digital/hmpps/managerecallsapi/controller/AddDocumentController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/managerecallsapi/controller/AddDocumentController.kt
@@ -4,7 +4,6 @@ import dev.forkhandles.result4k.map
 import dev.forkhandles.result4k.recover
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.http.HttpStatus.BAD_REQUEST
 import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
 import org.springframework.http.ResponseEntity
 import org.springframework.security.access.prepost.PreAuthorize
@@ -13,12 +12,10 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.server.ResponseStatusException
 import uk.gov.justice.digital.hmpps.managerecallsapi.db.RecallDocumentCategory
 import uk.gov.justice.digital.hmpps.managerecallsapi.documents.toBase64DecodedByteArray
 import uk.gov.justice.digital.hmpps.managerecallsapi.domain.RecallId
 import uk.gov.justice.digital.hmpps.managerecallsapi.service.RecallDocumentService
-import uk.gov.justice.digital.hmpps.managerecallsapi.service.RecallNotFoundException
 import java.net.URI
 import java.util.UUID
 
@@ -33,10 +30,10 @@ class AddDocumentController(
   @PostMapping("/recalls/{recallId}/documents")
   fun addDocument(
     @PathVariable("recallId") recallId: RecallId,
-    @RequestBody body: AddDocumentRequest
+    @RequestBody addDocumentRequest: AddDocumentRequest
   ): ResponseEntity<AddDocumentResponse> =
     // TODO:  Restrict the types of documents that can be uploaded. i.e. RECALL_NOTIFICATION, REVOCATION_ORDER
-    uploadDocument(recallId, body).map { documentId ->
+    uploadDocument(recallId, addDocumentRequest).map { documentId ->
       ResponseEntity
         .created(URI.create("$baseUri/recalls/$recallId/documents/$documentId"))
         .body(AddDocumentResponse(documentId = documentId))
@@ -46,18 +43,13 @@ class AddDocumentController(
 
   private fun uploadDocument(
     recallId: RecallId,
-    body: AddDocumentRequest
-  ) = try {
-    recallDocumentService.scanAndStoreDocument(
-      recallId = recallId,
-      documentBytes = body.fileContent.toBase64DecodedByteArray(),
-      documentCategory = body.category,
-      fileName = body.fileName
-    )
-  } catch (e: RecallNotFoundException) {
-    // Anyone object ot getting rid of this and letting the default NotFoundException return NOT_FOUND?
-    throw ResponseStatusException(BAD_REQUEST, e.message, e)
-  }
+    addDocumentRequest: AddDocumentRequest
+  ) = recallDocumentService.scanAndStoreDocument(
+    recallId = recallId,
+    documentBytes = addDocumentRequest.fileContent.toBase64DecodedByteArray(),
+    documentCategory = addDocumentRequest.category,
+    fileName = addDocumentRequest.fileName
+  )
 }
 
 data class AddDocumentRequest(
