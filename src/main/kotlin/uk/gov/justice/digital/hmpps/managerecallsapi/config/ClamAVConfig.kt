651330d10589c97b90180c3a161cd99e70330834
Mike Dunford <mike.dunford@digital.justice.gov.uk>
2021-10-13 07:32:33 +0100

    PUD-682:  Allow disabling clamav virus scan by setting clamav.virus.scan.enabled=false

--- src/main/kotlin/uk/gov/justice/digital/hmpps/managerecallsapi/config/ClamAVConfig.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/managerecallsapi/config/ClamAVConfig.kt
@@ -1,8 +1,14 @@
 package uk.gov.justice.digital.hmpps.managerecallsapi.config
 
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import uk.gov.justice.digital.hmpps.managerecallsapi.service.ClamavVirusScanner
+import uk.gov.justice.digital.hmpps.managerecallsapi.service.VirusScanResult.NoVirusFound
+import uk.gov.justice.digital.hmpps.managerecallsapi.service.VirusScanner
 import xyz.capybara.clamav.ClamavClient
 
 @Configuration
@@ -11,6 +17,28 @@ class ClamAVConfig(
   @Value("\${clamav.port}") val clamavPort: Int
 ) {
 
+  private val log = LoggerFactory.getLogger(this::class.java)
+
   @Bean
+  @ConditionalOnProperty(
+    value = ["clamav.virus.scan.enabled"],
+    havingValue = "true",
+    matchIfMissing = true
+  )
   fun clamavClient(): ClamavClient = ClamavClient(clamavHost, clamavPort)
+
+  @Bean
+  @ConditionalOnProperty(
+    value = ["clamav.virus.scan.enabled"],
+    havingValue = "true",
+    matchIfMissing = true
+  )
+  fun virusScanner(clamavClient: ClamavClient): VirusScanner = ClamavVirusScanner(clamavClient)
+
+  @Bean
+  @ConditionalOnMissingBean
+  fun stubVirusScanner() = VirusScanner {
+    log.info("Stub Virus Scanner returning NoVirusFound")
+    NoVirusFound
+  }
 }
