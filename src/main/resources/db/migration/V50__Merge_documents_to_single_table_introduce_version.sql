c9ed1ea2add2603fa927321983813aacd1f79b20
andrew.newton <andrew.newton@digital.justice.gov.uk>
2021-10-28 19:14:28 +0100

    PUD-862  add DB constraint on version null or non-null versus category

--- src/main/resources/db/migration/V50__Merge_documents_to_single_table_introduce_version.sql
+++ src/main/resources/db/migration/V50__Merge_documents_to_single_table_introduce_version.sql
@@ -7,8 +7,14 @@ CREATE TABLE document
     version   INTEGER,
     created_date_time TIMESTAMPTZ NOT NULL
 );
+-- Allow only one entry per recall, category and non-null version; allow many entries per recall, category and null version
 -- following only constrains where 'version' is non-null because of the SQL Standard feature that null != null, e.g. as per https://www.postgresql.org/docs/current/functions-comparison.html
 ALTER TABLE document ADD CONSTRAINT unique_recall_category_version_non_null UNIQUE (recall_id, category, version);
+
+-- Allow only valid values for version versus category
+ALTER TABLE document ADD CONSTRAINT version_constraint_by_category
+    CHECK ((version IS NULL AND category IN ('OTHER', 'UNCATEGORISED')) OR
+           (version IS NOT NULL AND category NOT IN ('OTHER', 'UNCATEGORISED')));
 
 ALTER TABLE versioned_document ADD COLUMN version INTEGER DEFAULT 1;
 INSERT INTO document(id, recall_id, category, file_name, version, created_date_time)
