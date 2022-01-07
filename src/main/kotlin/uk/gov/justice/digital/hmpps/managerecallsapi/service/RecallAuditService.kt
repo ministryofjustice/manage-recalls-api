4bd25201aa3dcaf37e17a14ca34881fb1757fc1e
Phil Segal <philsegal@users.noreply.github.com>
2022-01-07 09:18:44 +0000

    PUD-766: Endpoint to return summary of audit information for recall (#419)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/managerecallsapi/service/RecallAuditService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/managerecallsapi/service/RecallAuditService.kt
@@ -7,9 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import org.springframework.util.StringUtils
 import uk.gov.justice.digital.hmpps.managerecallsapi.controller.FieldAuditEntry
+import uk.gov.justice.digital.hmpps.managerecallsapi.controller.FieldAuditSummary
 import uk.gov.justice.digital.hmpps.managerecallsapi.db.Recall
 import uk.gov.justice.digital.hmpps.managerecallsapi.db.RecallAuditRepository
 import uk.gov.justice.digital.hmpps.managerecallsapi.db.RecallFieldAudit
+import uk.gov.justice.digital.hmpps.managerecallsapi.db.RecallFieldSummary
 import uk.gov.justice.digital.hmpps.managerecallsapi.domain.ColumnName
 import uk.gov.justice.digital.hmpps.managerecallsapi.domain.FieldName
 import uk.gov.justice.digital.hmpps.managerecallsapi.domain.FullName
@@ -23,14 +25,37 @@ class RecallAuditService(
   @Autowired private val recallAuditRepository: RecallAuditRepository,
   @Autowired private val entityManagerFactory: EntityManagerFactory
 ) {
-  var sessionFactory = entityManagerFactory.unwrap(SessionFactory::class.java)
-  var persister = sessionFactory.getClassMetadata(Recall::class.java) as AbstractEntityPersister
+  val sessionFactory: SessionFactory = entityManagerFactory.unwrap(SessionFactory::class.java)
+  val persister = sessionFactory.getClassMetadata(Recall::class.java) as AbstractEntityPersister
 
   fun getAuditForFieldName(recallId: RecallId, fieldName: FieldName): List<FieldAuditEntry> {
     val columnName = getColumnNameForField(fieldName)
     return recallAuditRepository.auditForRecallIdAndColumnName(recallId.value, columnName.value)
       .map { it.toFieldAuditEntry(fieldName) }
   }
+
+  fun getAuditSummaryForRecall(recallId: RecallId): List<FieldAuditSummary> {
+    return recallAuditRepository.auditSummaryForRecall(recallId.value)
+      .map { it.toFieldAuditSummary() }
+  }
+
+  private fun RecallFieldAudit.toFieldAuditEntry(fieldName: FieldName) =
+    FieldAuditEntry(
+      this.auditId,
+      RecallId(this.recallId),
+      FullName(this.updatedByUserName),
+      this.updatedDateTime.toLocalDateTime().atOffset(ZoneOffset.UTC),
+      translateValue(this.updatedValue, fieldName)
+    )
+
+  private fun RecallFieldSummary.toFieldAuditSummary() =
+    FieldAuditSummary(
+      this.auditId,
+      getFieldNameForColumn(ColumnName(this.columnName)),
+      FullName(this.updatedByUserName),
+      this.updatedDateTime.toLocalDateTime().atOffset(ZoneOffset.UTC),
+      this.auditCount
+    )
 
   private fun getColumnNameForField(fieldName: FieldName): ColumnName {
     val recallField = Recall::class.java.getDeclaredField(fieldName.value)
@@ -46,14 +71,11 @@ class RecallAuditService(
     )
   }
 
-  private fun RecallFieldAudit.toFieldAuditEntry(fieldName: FieldName) =
-    FieldAuditEntry(
-      this.auditId,
-      RecallId(this.recallId),
-      FullName(this.updatedByUserName),
-      this.updatedDateTime.toLocalDateTime().atOffset(ZoneOffset.UTC),
-      translateValue(this.updatedValue, fieldName)
-    )
+  private fun getFieldNameForColumn(columnName: ColumnName): FieldName {
+    return Recall::class.java.declaredFields.first { field ->
+      getColumnNameForField(FieldName(field.name)) == columnName
+    }.let { FieldName(it.name) }
+  }
 
   private fun translateValue(updatedValue: String, fieldName: FieldName): Any =
     when (fieldName) {
