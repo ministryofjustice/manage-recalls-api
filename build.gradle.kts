d0060b49932ee7b98a42891dd15be0324b829e9a
Phil Segal <philsegal@users.noreply.github.com>
2022-02-04 10:16:52 +0000

    Update to 4.0.3-beta to fix `CVE-2022-23181` (#471)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.0.1"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.0.3-beta"
   kotlin("plugin.spring") version "1.5.10"
   kotlin("plugin.jpa") version "1.5.20"
   kotlin("plugin.serialization") version "1.4.31"
