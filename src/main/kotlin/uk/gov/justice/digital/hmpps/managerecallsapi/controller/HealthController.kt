baf7253c6ab82b1ee9f0a0863a11519618a4920e
Darren Oakley <darren.oakley@digital.justice.gov.uk>
2022-02-22 09:04:31 +0000

    Make the `/health` endpoint return JSON. (#502)
    This is just to make it consistent with everything else and to also make
    it easier to test on the UI end.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/managerecallsapi/controller/HealthController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/managerecallsapi/controller/HealthController.kt
@@ -1,11 +1,19 @@
 package uk.gov.justice.digital.hmpps.managerecallsapi.controller
 
-import org.springframework.http.ResponseEntity
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping(produces = [MediaType.APPLICATION_JSON_VALUE])
 class HealthController() {
   @GetMapping("/health")
-  fun getHealth(): ResponseEntity<String> = ResponseEntity.ok("OK")
+  @ResponseStatus(HttpStatus.OK)
+  fun getHealth(): Map<String, String> =
+    hashMapOf(
+      "Status" to "UP"
+    )
 }
